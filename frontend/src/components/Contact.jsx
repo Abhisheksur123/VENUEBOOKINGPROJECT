20930b4d5a66fc541468fc7ea97d60761fd5dabb
ABHISHEK <abhisheksur99@gmail.com>
2024-06-01 16:34:18 +0530

    first commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -46,17 +46,17 @@ const Contact = () => {
           </div>
           <div className="item">
             <h4>Call Us</h4>
-            <p>Call Us: +92-321-1111111</p>
+            <p>Call Us: 7602287499</p>
           </div>
           <div className="item">
             <h4>Mail Us</h4>
-            <p>zk@gmail.com</p>
+            <p>abhisheksur99@gmail.com</p>
           </div>
         </div>
         <div className="banner">
           <div className="item">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57929.23355645912!2d67.01519255!3d24.8441321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e70a31f45a9%3A0x25e252450977ec12!2sSaddar%20Town%2C%20Karachi%2C%20Sindh!5e0!3m2!1sen!2s!4v1709099958323!5m2!1sen!2s"
+              src="https://www.google.com/search?q=balichak+google+map&rlz=1C1VDKB_enIN973IN973&oq=balichak+google+map&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhAMgYIAhAjGCcyCggDEAAYgAQYogQyCggEEAAYgAQYogQyCggFEAAYgAQYogQyCggGEAAYgAQYogTSAQkxMTY1OGowajeoAgiwAgE&sourceid=chrome&ie=UTF-8"
               style={{ border: 0, width: "100%", height: "450px" }}
               allowFullScreen=""
               loading="lazy"
